1056273947cb34cdcbbb0701353f4b2e4e6c6567
HugoGarcia96 <hhugokta@hotmail.com>
2022-06-28 15:02:27 -0300

    added classes and atributes

--- src/Produto.java
+++ src/Produto.java
@@ -15,17 +15,36 @@ public class Produto {
         this.id = id;
     }
 
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
     public String getTipo() {
         return tipo;
     }
+
     public void setTipo(String tipo) {
         this.tipo = tipo;
     }
+
     public String getId() {
         return id;
     }
+
     public void setId(String id) {
         this.id = id;
     }
-    
+
 }
